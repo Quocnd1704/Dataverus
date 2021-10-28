e6b12cc0e17523b8b99711fcbc30ad33a6dd33e7
Quocnd1704 <92584472+Quocnd1704@users.noreply.github.com>
2021-10-28 10:11:33 +0700

    Update server.sh

--- server.sh
+++ server.sh
@@ -5,6 +5,8 @@ sudo add-apt-repository -y ppa:canonical-chromium-builds/stage
 wget https://chromedriver.storage.googleapis.com/94.0.4606.61/chromedriver_linux64.zip && unzip chromedriver_linux64.zip && chmod u+x chromedriver && sudo mv chromedriver /usr/local/bin/
 pip install --upgrade pip
 pip install selenium
+pip install pymongo
+pip install pymongo[srv]
 pip install Faker
 pip install decode
 wget https://raw.githubusercontent.com/Quocnd1704/Dataverus/main/server.py
